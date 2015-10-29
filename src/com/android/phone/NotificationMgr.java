d983a5599c23722284eb213548d3c53c155dd09e
beanstown106 <nbenis106@gmail.com>
2015-10-29 02:48:10 -0400

    Make voicemail notifications dismissable on all carriers
    Change-Id: I4a61b181c32ad9ba1947761340207393c174b7af
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- src/com/android/phone/NotificationMgr.java
+++ src/com/android/phone/NotificationMgr.java
@@ -344,8 +344,8 @@ public class NotificationMgr {
                     .setContentText(notificationText)
                     .setContentIntent(pendingIntent)
                     .setColor(res.getColor(R.color.dialer_theme_color))
-                    .setOngoing(carrierConfig.getBoolean(
-                            CarrierConfigManager.KEY_VOICEMAIL_NOTIFICATION_PERSISTENT_BOOL))
+                    .setOngoing(false/*carrierConfig.getBoolean(
+                            CarrierConfigManager.KEY_VOICEMAIL_NOTIFICATION_PERSISTENT_BOOL)*/)
                     .setChannel(NotificationChannelController.CHANNEL_ID_VOICE_MAIL)
                     .setOnlyAlertOnce(isRefresh);
 
